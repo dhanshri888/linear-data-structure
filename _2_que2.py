27845241e52b2a6a37cf4bb6de5e5e64f8d1f7d1
dhanshri <112890281+dhanshri888@users.noreply.github.com>
2022-11-02 20:27:36 +0530

    DSA assignment

---
+++ _2_que2.py
@@ -0,0 +1,10 @@
+# 2. write a program to reverce an array in place ? in place means u cannot creat a new array
+
+
+# lst = [3,4,5,6,7,8,9]
+
+lst = list(input("Enter the value to reverse :"))
+
+lst.reverse()
+
+print(lst)
